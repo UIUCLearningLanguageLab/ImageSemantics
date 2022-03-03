09077e7c9f219e27246f15868003e520f4c909c9
jwillits@illinois.edu <jwillits@illinois.edu>
2022-03-03 13:02:14 -0600

    new code

--- extract_images/src/save_frames.py
+++ extract_images/src/save_frames.py
@@ -4,6 +4,7 @@ import time
 import csv
 import shutil
 import sys
+import random
 
 
 class VideoObject:
@@ -17,7 +18,7 @@ class VideoObject:
 
 		self.video = None
 		self.fps = None
-		self.frame_rate = None
+		self.rounded_frame_rate = None
 		self.num_frames = None
 		self.length = None
 
@@ -28,6 +29,7 @@ class VideoObject:
 		# try statement here is basically checking to make sure the directory named after this movie doesnt already exit
 		try:
 			os.makedirs(self.image_path)
+			success = True
 		except:
 			print("Could not make directory {}".format(self.image_path))
 			sys.exit()
@@ -36,62 +38,49 @@ class VideoObject:
 	def load_video(self):
 		self.video = cv2.VideoCapture(self.mp4_path)     # a built in command from cv2 that creates a video object from the video
 		self.fps = self.video.get(cv2.CAP_PROP_FPS)      # saving the frames per second of the video
-		self.frame_rate = round(self.fps)			# rounding fps to an int
+		self.rounded_frame_rate = round(self.fps)			# rounding fps to an int
 		self.num_frames = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))		# getting the total number of frames in the video
+		
 		self.length = (self.num_frames/self.fps)/60				# getting the length of the video
+		
 
 	def save_frames(self, images_per_second, window_size):
 		
+		print("Saving frames for video {}".format(self.video_name))
+		print("		frames per second: {}".format(self.fps))
+		print("		rounded frame rate: {}".format(self.rounded_frame_rate))
+		print("		num frames in video: {}".format(self.num_frames))
+		print("		video length: {}".format(self.length))
+
+		image_list = []
 		success, image = self.video.read()   # cv2 command that reads the first frame of the video, successful, the image is stored in image, 
 						     # and success is a boolean telling us if it succeeded
-		image_list = []  # a list for keeping track of a set of images so we can pick the least blurry one
 		
-		# if the first retrieval was a success add it to the list
-		if success:
-			image_list.append(image)
-		
-		 #  while an image continues to be sucessfully extracted from the video, add them to the list
+		i = 0
 		while success:
-			success,image = self.video.read()
 			image_list.append(image)
-		
-		num_images = len(image_list)
-		for i in range(num_images):
-			
-			# checks to see if i (the counter for where we are in the image list) is one we want, based on how often we wanted to sample images
-			# for example, if the (rounded to int) frame rate was 30 fps, and we wanted 1 image every five seconds (ips = 1/5) 
-			# then denominator is 30/(1/5) = 150, meaning we want to grab every 150th frame
-			if i % (self.frame_rate/images_per_second) == 0:
-				
-				# if window size is 0, we are skipping the pick least blurry step and saving this image regardless
-				if window_size == 0:
-					image = image_list[i]
-					image_index = i
-					
-				# if window size is greater than 0, we want to use that window size and pick the least blurry one
-				elif window_size > 0:
-					
-					# get the start and stop index for what images we want to get based on the current i and window size
-					# this list will always be window size long, unless the window would take us before or after the end of 
-					# 	image list, in which case it will truncate it
-					start_index = int(i-window_size/2)
-					if start_index < 0:
-						start_index = 0
-					stop_index = int(i+window_size/2)+1
-					if stop_index > num_images-1:
-						stop_index = num_images-1
-			
-					# get the window of images, and choose the least blurry one
-					image_window = image_list[start:stop]
-					image, window_index = self.get_least_blurry_image(image_window)
-					image_index = i-window_size/2+window_index
-					
-				save_single_image(image, image_index)
+			if len(image_list) > window_size:
+				image_list.pop()
+
+			if i % (self.rounded_frame_rate/images_per_second) == 0:
+				if (len(image_list) == window_size):
+					if window_size > 1:
+						image_to_save, window_index = self.get_least_blurry_image(image_list)
+						image_index = i-window_size+window_index
+					else:
+						image_index = i
+						image_to_save = image
+
+					self.save_single_image(image_to_save, image_index)
+
+			success,image = self.video.read()
+			i += 1
 	
 	def get_least_blurry_image(self, image_window):
 		# Grace, your code goes here, return the least blurry image, and the index of which image in image_window that it was
-		pass
-	
+		index = random.randint(0,4)
+		return image_window[index], index
+		
 	def save_single_image(self, image, index):
 		string_index = str(index)
 		while len(string_index) < 6:
@@ -103,26 +92,28 @@ class VideoObject:
 
 def get_video_object_list(video_list_file):
 	''' create a dictionary of video names pointing to video objects '''
+	file_list = os.listdir("../mp4_videos/")
 	video_object_dict = {}
-	with open(video_list_file) as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=',')
-		for row in csv_reader:
-			new_video_object = VideoObject(row[0])
-			video_object_dict[row[0]] = new_video_object
+
+	for file_name in file_list:
+		if file_name[0] != '.':
+			new_video_object = VideoObject(file_name)
+			video_object_dict[file_name] = new_video_object
+			
 	return video_object_dict
 
 
 def main():
 	images_per_second = 1/5  # a fraction of 1/n means that an image will be gathered every n seconds, a whole number means n images per second
-	window_size = 10   # the number of images out of which we will choose the least blurry
-	video_list_file = "../video_list1.csv"
+	window_size = 1   # the number of images out of which we will choose the least blurry
+	video_list_file = "../video_list.csv"
 	
 	# this creates a list of video objects, which are instances of the video object class defined above.
 	# This class is basically a cv2 video object, plus some associated information we are keeping track of, like the video name, file path, etc.
 	# the movies that will be added to the list are specified in the video list file
 	video_object_dict = get_video_object_list(video_list_file)
 	
-	# for each video in the list, we are going to save frames from it according to the images_per_second variable
+	# # for each video in the list, we are going to save frames from it according to the images_per_second variable
 	for video_name in video_object_dict:
 		video_object = video_object_dict[video_name]
 		video_object.save_frames(images_per_second, window_size)
