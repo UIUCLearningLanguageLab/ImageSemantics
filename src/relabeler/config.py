6097505044fe34d03b016e0bbe3ef080620831ec
Jon Willits <jwillits@illinois.edu>
2023-03-24 13:26:12 -0500

    Create config.py

---
+++ src/relabeler/config.py
@@ -0,0 +1,10 @@
+
+class Config:
+    main_window_dimensions = (1720, 1000)  # the width and height of the main window
+    small_image_dimensions = (176, 99)  # the dimensions of the preview images
+    large_image_scale = 4.6  # how much bigger the big image is than the smaller ones
+    interface_frame_width = 160  # the width of the left panel of UI elements
+    mini_frame_height = 240  # the height of each of the first three subsets of UI elements
+    big_image_y_spacing = 10  # the amount of space between the two big images on the right
+    num_image_rows = 9  # the number of preview image rows
+    num_image_columns = 4  # the number of preview image columns
