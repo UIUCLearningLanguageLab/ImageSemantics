abee9d5bf6183df66d1e19ec5ca2e51e1995601f
Jon Willits <jwillits@illinois.edu>
2023-10-04 09:02:14 -0500

    Update config.py

--- src/relabeler/config.py
+++ src/relabeler/config.py
@@ -1,9 +1,9 @@
 
 class Config:
-    main_window_dimensions = (1750, 1000)  # the width and height of the main window
-    small_image_dimensions = (176, 99)  # the dimensions of the preview images
-    large_image_scale = 4.76  # how much bigger the big image is than the smaller ones
+    main_window_dimensions = (2250, 1300)  # the width and height of the main window
+    small_image_dimensions = (168, 96)  # the dimensions of the preview images
+    large_image_scale = 6.45  # how much bigger the big image is than the smaller ones
     interface_frame_width = 160  # the width of the left panel of UI elements
     big_image_y_spacing = 10  # the amount of space between the two big images on the right
-    num_image_rows = 9  # the number of preview image rows
+    num_image_rows = 12  # the number of preview image rows
     num_image_columns = 4  # the number of preview image columns
