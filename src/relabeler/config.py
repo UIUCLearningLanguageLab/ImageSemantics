723ceb0af76cdc45b3a5b1dcd0b54adf06fdb96d
Jon Willits <jwillits@illinois.edu>
2023-10-10 09:17:46 -0500

    changed config

--- src/relabeler/config.py
+++ src/relabeler/config.py
@@ -3,5 +3,5 @@ class Config:
     main_window_dimensions = (1920, 1280)  # the width and height of the main window
     interface_frame_height = 70  # the width of the left panel of UI elements
     image_dimensions = (1920, 1080)
-    num_image_previews = (8, 5)
+    num_image_previews = (10, 5)
     comment_image_dimensions = (1366, 768)
