5f6976977e1aa9e9b12fa9acc935b821767383b7
Jon Willits <jwillits@illinois.edu>
2023-10-10 14:55:05 -0500

    change display proportions

--- src/relabeler/config.py
+++ src/relabeler/config.py
@@ -3,5 +3,5 @@ class Config:
     main_window_dimensions = (1920, 1280)  # the width and height of the main window
     interface_frame_height = 70  # the width of the left panel of UI elements
     image_dimensions = (1920, 1080)
-    num_image_previews = (9, 5)
+    num_image_previews = (10, 4)
     comment_image_dimensions = (1366, 768)
